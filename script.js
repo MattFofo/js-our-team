2e79ce0ed94cc99b2da934c1f16da29c342ac456
MattFofo <matteo.segattini2@gmail.com>
2022-03-07 15:49:54 +0100

    sistemato layout pagina

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Viene fornito un layout di base. Come prima cosa nel file js definitevi un array
 Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
 */
 
-const eleTeamContainer = document.querySelector(".team-container");
+
 
 // arrey di oggetti che rappresentano i membri del team
 const arrUsersData = [
@@ -48,26 +48,43 @@ const arrUsersData = [
 console.log('arrUsersData', arrUsersData)
 
 
+//sistemo layout div.team-container
+const eleTeamContainer = document.querySelector(".team-container");
+eleTeamContainer.style.justifyContent = "flex-start"; 
+
 //ciclo per scorrere gli oggetti dell'arrey
 for (const key in arrUsersData) {
     console.log(key, arrUsersData[key])
 
     //creo un elemento per ogni oggetto dell'arrey e inserisco i dati membri
-    let ele = document.createElement('div')
-    ele.innerHTML = `          
-    <div class="team-card">
-        <div class="card-image">
-            <img
-                src= ${arrUsersData[key]["img"]}
-                alt= ${arrUsersData[key]["name"]}
-            />
-        </div>
-        <div class="card-text">
-            <h3>${arrUsersData[key]["name"]}</h3>
-            <p>${arrUsersData[key]["role"]}</p>
-        </div>
-    </div>`
-    
-    eleTeamContainer.append(ele);
+    createEleCard();
+
+    function createEleCard() {
+        let ele = document.createElement('div')
+        ele.innerHTML = `          
+        <div class="team-card">
+            <div class="card-image">
+                <img
+                    src= ${arrUsersData[key]["img"]}
+                    alt= ${arrUsersData[key]["name"]}
+                />
+            </div>
+            <div class="card-text">
+                <h3>${arrUsersData[key]["name"]}</h3>
+                <p>${arrUsersData[key]["role"]}</p>
+            </div>
+        </div>`
+        
+        eleTeamContainer.append(ele);
+        const eleImges = document.getElementsByTagName("img");
+        console.log(eleImges[0])
+        
+        //ciclo per ridimensionare immagini
+        for (let i = 0; i < eleImges.length; i++) {
+            
+            eleImges[i].style.width = "370px";
+            
+        }
+    }
 
 }
